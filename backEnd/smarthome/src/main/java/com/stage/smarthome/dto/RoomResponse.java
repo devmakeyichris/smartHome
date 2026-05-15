4987f31a929f13b0996a7204c6c6714b126c2f62
chris <devmakeyichris@gmil.com>
2026-05-15 14:03:21 +0100

    ✅ back : ajout dto

---
+++ backEnd/smarthome/src/main/java/com/stage/smarthome/dto/RoomResponse.java
@@ -0,0 +1,46 @@
+package com.stage.smarthome.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.stage.smarthome.entity.Room;
+
+public class RoomResponse {
+    private Long id;
+    private String name;
+    private List<DeviceResponse> devices;
+
+    public RoomResponse(Room room) {
+        this.id = room.getId();
+        this.name = room.getName();
+        this.devices = room.getDevices()
+                           .stream()
+                           .map(DeviceResponse::new)
+                           .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<DeviceResponse> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<DeviceResponse> devices) {
+        this.devices = devices;
+    }
+}
+
