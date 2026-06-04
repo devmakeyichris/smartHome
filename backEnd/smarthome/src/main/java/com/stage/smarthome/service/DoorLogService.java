e81d264e72c4c863a29b7b77d40e1ca7bf64e189
chris <devmakeyichris@gmil.com>
2026-06-04 23:01:12 +0100

    ✅ back update

--- backEnd/smarthome/src/main/java/com/stage/smarthome/service/DoorLogService.java
+++ backEnd/smarthome/src/main/java/com/stage/smarthome/service/DoorLogService.java
@@ -8,29 +8,32 @@ import org.springframework.stereotype.Service;
 
 import com.stage.smarthome.entity.Device;
 import com.stage.smarthome.entity.DoorLog;
+import com.stage.smarthome.entity.RfidCard;
 import com.stage.smarthome.repository.DoorLogRepository;
 
 @Service
 public class DoorLogService {
     @Autowired
     private DoorLogRepository doorLogRepository;
-
-    public DoorLog saveLog(Device door, String action) {
+    
+    
+    public DoorLog saveLog(Device door, String action, RfidCard card) {
         DoorLog log = new DoorLog();
         log.setDoor(door);
         log.setAction(action);
+        log.setCard(card);
         log.setTimestamp(LocalDateTime.now());
         return doorLogRepository.save(log);
     }
-
+    
     public List<DoorLog> getLogsForDoor(Device door) {
         return doorLogRepository.findByDoor(door);
     }
-
+    
     public DoorLogRepository getDoorLogRepository() {
         return doorLogRepository;
     }
-
+    
     public void setDoorLogRepository(DoorLogRepository doorLogRepository) {
         this.doorLogRepository = doorLogRepository;
     }
